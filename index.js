b1d63dea2f6ca874106a004585f912984ffb84ba
BryancevSA <steperb@gmail.com>
2023-01-28 21:06:32 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+import { Console } from 'console';
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello it is DSA!');
+});
+
+app.listen(5000, (err) => {
+    if (err) {
+        return console.log(err);
+    }
+
+    console.log("Server is ok");
+});
+
