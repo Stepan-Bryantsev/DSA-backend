819340e7a0f332e7a10b08be85f3d87d8988bd37
BryancevSA <steperb@gmail.com>
2023-03-26 06:43:00 +0300

    BIO FIX

--- src/utils/accountValidations.ts
+++ src/utils/accountValidations.ts
@@ -4,5 +4,5 @@ export const accountValidation = [
   body("email", "Wrong email format").optional().isEmail(),
   body("fullName", "Wrong full name").optional().isLength({ min: 3 }),
   body("facultyId", "Wrong faculty format").optional().isNumeric(),
-  body("bio", "Wrong bio format").optional().isNumeric(),
+  body("bio", "Wrong bio format").optional(),
 ];
